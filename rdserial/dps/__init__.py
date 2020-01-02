698b85ca38c707ea1951b789362bdde0df5a3054
Ryan Finnie <ryan@finnie.org>
2020-01-01 17:56:52 -0800

    dps: Simplify all registers

--- rdserial/dps/__init__.py
+++ rdserial/dps/__init__.py
@@ -31,91 +31,85 @@ def _simple_int(multiple=1):
     }
 
 
+def _simple_bool():
+    return {
+        'from_int': lambda x: bool(x),
+        'to_int': lambda x: int(x),
+    }
+
+
 class DPSDeviceState:
     def __init__(self, collection_time=None):
         self.register_properties = {
             'setting_volts': {
                 'description': 'Voltage setting',
                 'register': 0x00,
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'setting_amps': {
                 'description': 'Amperage setting',
                 'register': 0x01,
-                'from_int': lambda x: x / 1000,
-                'to_int': lambda x: int(x * 1000),
+                **_simple_int(1000),
             },
             'volts': {
                 'description': 'Output volts',
                 'register': 0x02,
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'amps': {
                 'description': 'Output amps',
                 'register': 0x03,
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'watts': {
                 'description': 'Output watts',
                 'register': 0x04,
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'input_volts': {
                 'description': 'Input volts',
                 'register': 0x05,
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'key_lock': {
                 'description': 'Key lock',
                 'register': 0x06,
-                'from_int': lambda x: bool(x),
-                'to_int': lambda x: int(x),
+                **_simple_bool(),
             },
             'protection': {
                 'description': 'Protection status',
                 'register': 0x07,
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'constant_current': {
                 'description': 'Constant current mode',
                 'register': 0x08,
-                'from_int': lambda x: bool(x),
-                'to_int': lambda x: int(x),
+                **_simple_bool(),
             },
             'output_state': {
                 'description': 'Output state',
                 'register': 0x09,
-                'from_int': lambda x: bool(x),
-                'to_int': lambda x: int(x),
+                **_simple_bool(),
             },
             'brightness': {
                 'description': 'Brightness level',
                 'register': 0x0a,
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'model': {
                 'description': 'Device model',
                 'register': 0x0b,
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'firmware': {
                 'description': 'Device firmware',
                 'register': 0x0c,
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'group_loader': {
                 'description': 'Group loader',
                 'register': 0x23,
-                'from_int': lambda x: 0,
+                'from_int': lambda x: 0,  # Write-only
                 'to_int': lambda x: int(x),
             },
         }
@@ -147,50 +141,42 @@ class DPSGroupState:
             'setting_volts': {
                 'description': 'Voltage setting',
                 'register': 0x50 + (0x10 * group),
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'setting_amps': {
                 'description': 'Amperage setting',
                 'register': 0x51 + (0x10 * group),
-                'from_int': lambda x: x / 1000,
-                'to_int': lambda x: int(x * 1000),
+                **_simple_int(1000),
             },
             'cutoff_volts': {
                 'description': 'Volts cutoff',
                 'register': 0x52 + (0x10 * group),
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'cutoff_amps': {
                 'description': 'Amps cutoff',
                 'register': 0x53 + (0x10 * group),
-                'from_int': lambda x: x / 1000,
-                'to_int': lambda x: int(x * 1000),
+                **_simple_int(1000),
             },
             'cutoff_watts': {
                 'description': 'Watts cutoff',
                 'register': 0x54 + (0x10 * group),
-                'from_int': lambda x: x / 10,
-                'to_int': lambda x: int(x * 10),
+                **_simple_int(10),
             },
             'brightness': {
                 'description': 'Brightness level',
                 'register': 0x55 + (0x10 * group),
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'maintain_output': {
                 'description': 'Maintain output state during group change',
                 'register': 0x56 + (0x10 * group),
-                'from_int': lambda x: bool(x),
-                'to_int': lambda x: int(x),
+                **_simple_bool(),
             },
             'poweron_output': {
                 'description': 'Enable output on power-on',
                 'register': 0x57 + (0x10 * group),
-                'from_int': lambda x: bool(x),
-                'to_int': lambda x: int(x),
+                **_simple_bool(),
             },
         }
 
@@ -216,97 +202,82 @@ class RDDeviceState:
             'model': {
                 'description': 'Device model',
                 'register': 0x00,
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'serial': {
                 'description': 'Device serial',
                 'register': 0x02,  # 0x01 high?
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'firmware': {
                 'description': 'Device firmware',
                 'register': 0x03,
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'fan_temp_c': {
                 'description': 'Fan start temperature (C)',
                 'register': 0x05,  # 0x04 high?
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'fan_temp_f': {
                 'description': 'Fan start temperature (F)',
                 'register': 0x07,  # 0x06 high?
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'setting_volts': {
                 'description': 'Voltage setting',
                 'register': 0x08,
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'setting_amps': {
                 'description': 'Amperage setting',
                 'register': 0x09,
-                'from_int': lambda x: x / 1000,
-                'to_int': lambda x: int(x * 1000),
+                **_simple_int(1000),
             },
             'volts': {
                 'description': 'Output volts',
                 'register': 0x0a,
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'amps': {
                 'description': 'Output amps',
                 'register': 0x0b,
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'watts': {
                 'description': 'Output watts',
                 'register': 0x0d,  # 0x0c high?
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'input_volts': {
                 'description': 'Input volts',
                 'register': 0x0e,
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'key_lock': {
                 'description': 'Key lock',
                 'register': 0x0f,
-                'from_int': lambda x: bool(x),
-                'to_int': lambda x: int(x),
+                **_simple_bool(),
             },
             'protection': {
                 'description': 'Protection status',
                 'register': 0x10,
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'constant_current': {
                 'description': 'Constant current mode',
                 'register': 0x11,
-                'from_int': lambda x: bool(x),
-                'to_int': lambda x: int(x),
+                **_simple_bool(),
             },
             'output_state': {
                 'description': 'Output state',
                 'register': 0x12,
-                'from_int': lambda x: bool(x),
-                'to_int': lambda x: int(x),
+                **_simple_bool(),
             },
             'group_loader': {
                 'description': 'Group loader',
                 'register': 0x13,
-                'from_int': lambda x: 0,
+                'from_int': lambda x: 0,  # Write-only
                 'to_int': lambda x: int(x),
             },
             # 0x14 - 0x2f: All 0
@@ -314,17 +285,23 @@ class RDDeviceState:
             'temp_c': {
                 'description': 'Temperature (C)',
                 'register': 0x23,  # 0x22 high?
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'temp_f': {
                 'description': 'Temperature (F)',
                 'register': 0x25,  # 0x24 high?
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
-            # 0x26 / 0x27 mAh
-            # 0x28 / 0x29 mWh
+            'cumulative_charge': {
+                'description': 'Cumulative charge (Ah)',
+                'register': 0x27,  # 0x26 high?
+                **_simple_int(1000),
+            },
+            'cumulative_energy': {
+                'description': 'Cumulative energy (Wh)',
+                'register': 0x29,  # 0x28 high?
+                **_simple_int(1000),
+            },
             'datetime_year': {'description': 'Year', 'register': 0x30, **_simple_int()},
             'datetime_month': {'description': 'Month', 'register': 0x31, **_simple_int()},
             'datetime_day': {'description': 'Day', 'register': 0x32, **_simple_int()},
@@ -334,20 +311,17 @@ class RDDeviceState:
             'brightness': {
                 'description': 'Brightness level',
                 'register': 0x48,
-                'from_int': lambda x: x,
-                'to_int': lambda x: int(x),
+                **_simple_int(),
             },
             'ovp': {
                 'description': 'Over-voltage limit (V)',
                 'register': 0x52,
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'ocp': {
                 'description': 'Over-current limit (A)',
                 'register': 0x53,
-                'from_int': lambda x: x / 1000,
-                'to_int': lambda x: int(x * 1000),
+                **_simple_int(1000),
             },
         }
 
@@ -378,26 +352,22 @@ class RDGroupState:
             'setting_volts': {
                 'description': 'Voltage setting',
                 'register': 0x50 + (0x04 * group),
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'setting_amps': {
                 'description': 'Amperage setting',
                 'register': 0x51 + (0x04 * group),
-                'from_int': lambda x: x / 1000,
-                'to_int': lambda x: int(x * 1000),
+                **_simple_int(1000),
             },
             'cutoff_volts': {
                 'description': 'Volts cutoff',
                 'register': 0x52 + (0x04 * group),
-                'from_int': lambda x: x / 100,
-                'to_int': lambda x: int(x * 100),
+                **_simple_int(100),
             },
             'cutoff_amps': {
                 'description': 'Amps cutoff',
                 'register': 0x53 + (0x04 * group),
-                'from_int': lambda x: x / 1000,
-                'to_int': lambda x: int(x * 1000),
+                **_simple_int(1000),
             },
         }
 
